221d325e17b557733b14ac4e84762977989facc5
Larkinator <slarkin729@googlemail.com>
2025-03-25 16:05:33 +0000

    alternate_Case code completed

--- src/alternate_case.py
+++ src/alternate_case.py
@@ -1,4 +1,54 @@
 def alternate_case(word):
+    result = []
+    uppercase = True        #first char will be upper 
+
+    for char in word:
+        if char.isalpha():      #if letter (not space)
+            result.append(char.upper() if uppercase else char.lower())  #if upper is true add upper letter to list, else add lower letter to list
+            uppercase = not uppercase       # switch to False 
+        else:
+            result.append(char)             # ie. for spaces 
+    return "".join(result)                  # returning a joined up string (not a list)
+
+
+
+
+
+
+
+#Past attempts
+
+#def alternate_case(word):
+  # new_word = ''
+    # uppercase = True  
+    
+    # for char in word:
+    #     if char == ' ':
+    #         new_word += ' '
+    #         uppercase = True  
+    #         continue
+            
+    #     if uppercase:
+    #         new_word += char.upper()
+    #     else:
+    #         new_word += char.lower()
+    #     uppercase = not uppercase
+    
+    # return new_word
+
+
+# def alternate_case(word):
+#     new_word = ''
+#     for index, char in enumerate(word):
+#         if index % 2 ==0:
+#             new_word += char.upper()
+#         else:
+#             new_word += char.lower()
+#         if char != 1:
+#             continue
+#     return new_word
+
+
     # new_word = ''
     # uppercase = True
     # for char in word:
@@ -12,19 +62,6 @@ def alternate_case(word):
     # return new_word
 
 
-
-
-
-    new_word = ''
-    for index, char in enumerate(word):
-        if index % 2 ==0:
-            new_word += char.upper()
-        else:
-            new_word += char.lower()
-        if char != 1:
-            
-            continue
-    return new_word
 
 
     # for char in word:
