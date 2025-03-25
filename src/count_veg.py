ace7f4b8e5f806bb9a66a2db2b408f932c583b41
deepkhd3 <deepkhd3@gmail.com>
2025-03-25 10:47:35 +0000

    count_veg complete

--- src/count_veg.py
+++ src/count_veg.py
@@ -1,7 +1,13 @@
-def count_veg(veg, type):
-    total = 0 
-    for item in veg: 
-        if item["type"] == 'root':
+def count_veg(veg_list, veg_type):
+    valid_types = ['root', 'leaf', 'legume', 'bulb', 'brassica']
+    if veg_type not in valid_types:
+        return 'Invalid type'
+    if not veg_list:
+        return 'Invalid list'
+    total = 0
+    for item in veg_list: 
+        if item["type"] == veg_type:
             total += item['quantity']
-            return item['quantity']
-    return 0 
+            
+    return total
+
--- test/test_count_veg.py
+++ test/test_count_veg.py
@@ -25,13 +25,63 @@ def test_if_multiple_type_is_root_sum_quantity():
 
     assert result == expected
 
-# def test_if_list_and_type_are_empty_return_zero(): 
-#     test_input_veg = []
-#     test_input_type = ''
-#     expected = 0
+def test_with_different_veg_types():
+    test_input_veg = [{"name": 'Parsnip', "type": 'root', "quantity": 4},
+  {"name": 'Broccoli', "type": 'brassica', "quantity": 1},
+  {"name": 'Carrot', "type": 'root', "quantity": 5},
+  {"name": 'Onion', "type": 'bulb', "quantity": 3},
+  {"name": 'Chard', "type": 'leaf', "quantity": 3},
+  {"name": 'Runner beans', "type": 'legume', "quantity": 8}]
+    test_input_type = 'leaf'
+    expected = 3
+    result = count_veg(test_input_veg, test_input_type)
+    assert result == expected
 
-#     result = count_veg(test_input_veg, test_input_type)
+def test_if_list_and_type_are_empty_return_invalid(): 
+    test_input_veg = []
+    test_input_type = ''
+    expected = 'Invalid type'
 
-#     assert result == expected
+    result = count_veg(test_input_veg, test_input_type)
+
+    assert result == expected
+
+def test_if_only_list_empty_return_invalid(): 
+    #arrange
+    test_input_veg = []
+    test_input_type = 'root'
+    expected = 'Invalid list'
+
+    #act 
+    result = count_veg(test_input_veg, test_input_type)
+
+    assert result == expected
+    
+def test_if_only_type_is_empty_return_invalid(): 
+    #arrange
+    test_input_veg = [{"name": 'Parsnip', "type": 'root', "quantity": 4}, 
+                      {"name": 'Carrot', "type": 'root', "quantity": 5}]
+    test_input_type = ''
+    expected = 'Invalid type'
+
+    #act 
+    result = count_veg(test_input_veg, test_input_type)
+
+    assert result == expected
+
+def test_if_invalid_veg_typ_return_invalid(): 
+    #arrange
+    test_input_veg = [{"name": 'Parsnip', "type": 'root', "quantity": 4}, 
+                      {"name": 'Carrot', "type": 'root', "quantity": 5}]
+    test_input_type = 'fruit'
+    expected = 'Invalid type'
+
+    #act 
+    result = count_veg(test_input_veg, test_input_type)
+
+    assert result == expected
+
+
+
     
    
